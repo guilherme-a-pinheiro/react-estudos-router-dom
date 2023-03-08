4fdf34c790c70ad3a5f6ddc3f285e535cb9db50b
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-03-07 23:52:30 -0300

    firsts routes

--- router/v6/src/main.tsx
+++ router/v6/src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './components/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './styles/global.css'
+import './styles/global.css';
+
+import { Home } from './components/Home';
+import { About } from './components/About';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
