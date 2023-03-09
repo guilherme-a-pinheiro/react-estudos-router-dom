42b9ba79c04e1ff20bb2e82887bfa55821879c82
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-03-09 00:12:18 -0300

    menu links config

--- router/v6/src/main.tsx
+++ router/v6/src/main.tsx
@@ -5,10 +5,12 @@ import './styles/global.css';
 
 import { Home } from './components/Home';
 import { About } from './components/About';
+import { Menu } from './components/Menu';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
